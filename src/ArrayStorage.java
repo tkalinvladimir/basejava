61759ddff764243183be4b0444efe600005770e8
Ткалин Владимир <tkalin.vladimir@gmail.com>
2019-01-14 16:37:36 +1000

    done HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,16 +4,21 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[3];
+    int size;
 
     void clear() {
         for (int i = 0; i < storage.length; i++) {
-            storage[i] = null;
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
         }
+        size = 0;
     }
 
     void save(Resume resume) {
-        storage[size()] = resume;
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
@@ -26,16 +31,19 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int size = size();
         boolean beenDeleted = false;
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
+            if (!beenDeleted && storage[i].uuid.equals(uuid)) {
                 beenDeleted = true;
+                size--;
             }
-            if (beenDeleted && storage[i] != null) {
-                storage[i-1] = storage[i];
-                storage[i] = null;
+            if (beenDeleted) {
+                if (i == size) {
+                    storage[i] = null;
+                } else {
+                    storage[i] = storage[i+1];
+                    storage[i+1] = null;
+                }
             }
         }
     }
@@ -44,15 +52,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                return i;
-            }
-        }
-        return storage.length;
+        return size;
     }
 }
